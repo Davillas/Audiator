c8867520e10ee337489c4b3e535e608fd2313058
David Akopyan <dg.akopyan@gmail.com>
2023-07-12 13:17:54 +0400

    Initialization of Avalonia xplat Pattern

---
+++ Audiator/Audiator/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Audiator.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    #region Title : string - Title
+
+    [ObservableProperty]
+    /// <summary>Title</summary>
+    private string? _Title = "Audiator";
+
+    #endregion
+}
---
+++ Audiator/Audiator/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Audiator.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
